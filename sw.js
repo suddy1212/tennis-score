abaee4bee1df0f33c8a185db98459f470ca186da
suddy1212 <suddy12plus@gmail.com>
2025-08-10 20:22:00 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'tennis-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(res => res || fetch(e.request).then(r => {
+        const copy = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return r;
+      }).catch(() => caches.match('./index.html')))
+    );
+  }
+});
